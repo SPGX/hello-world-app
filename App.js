0f788ef010f09a2322220ec25b591d2a8864aea3
SPGX <armsrichan2@gmail.com>
2020-09-17 14:55:22 +0700

    ห

--- App.js
+++ App.js
@@ -1,12 +1,36 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React , {useState} from 'react';
+import { StyleSheet, Text, View, Image, TextInput, Button } from 'react-native';
+import MyButton from './componensts/components.js'
 
 export default function App() {
+  const [ input, setInput] = useState('')
+  const [color, setColor] = useState('')
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Image
+        source={{ uri: 'https://www.igeargeek.com/_nuxt/img/835647d.png'}}
+        style={styles.logo}
+      />
+      <Text style={styles.text1}>
+        Hello World
+      </Text>
+      
+      <TextInput 
+        value={input}
+        onChangeText={(text) => { setInput(text) }}
+        placeholder=" Input here"
+        style={styles.input}
+      />
+      <StatusBar style="auto"/>
+
+      <Text style={{fontSize: 18, marginTop: 20, color: color}} >{input}</Text>
+      <View style={styles.buttonhole}>
+        <MyButton title="Red" color="red" onChangeColor={(color) => { setColor(color) }} />
+        <MyButton title="Blue" color="blue" onChangeColor={(color) => { setColor(color) }} />
+        <MyButton title="Green" color="green" onChangeColor={(color) => { setColor(color) }} />
+      </View>
     </View>
   );
 }
@@ -17,5 +41,26 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  text1: {
+    fontSize: 30,
+    color: '#000',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  logo: {
+    width: 150,
+    height: 150,
+  },
+  input: {
+    height: 40,
+    borderColor: 'gray',
+    borderStyle: 'solid',
+    borderWidth: 1,
+    width: '90%',
+  },
+  buttonhole: {
+    flexDirection: 'row',
+    marginTop: '20',
   },
 });
